b14af601ba8b26404a59482561b7e8fd34b5711b
igrek51 <igrek51.dev@gmail.com>
2022-04-09 22:10:11 +0200

    Speed up plotting tracks

--- looper/runner/plot.py
+++ looper/runner/plot.py
@@ -1,36 +1,35 @@
 import io
+import random
 
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 from starlette.responses import StreamingResponse
-from looper.debug.performance import measure_duration
 from looper.runner.looper import Looper
 
 from looper.runner.track import Track
 
 
 def generate_track_plot(track: Track, looper: Looper) -> StreamingResponse:
+    matplotlib.rcParams["agg.path.chunksize"] = 10_000
+    matplotlib.style.use("fast")
+
     if len(track.loop_chunks) == 0:
-        # all_chunks = looper.dsp.silence()
-        all_chunks = np.random.random(100_000)
+        all_chunks = looper.dsp.silence()
     else:
-        with measure_duration(f'concatenating track {track.index}'):
-            all_chunks = np.concatenate(track.loop_chunks)
-        with measure_duration(f'scaling track {track.index}'):
-            all_chunks = all_chunks / looper.config.max_amplitude
+        all_chunks = np.concatenate(track.loop_chunks)
+        all_chunks = all_chunks / looper.config.max_amplitude
 
     figure = plt.figure()
-    figure.set_size_inches((1260-52)/100, (320-29)/100)
+    figure.set_size_inches((1260 - 52) / 100, (320 - 29) / 100)
     figure.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-    plt.margins(0,0)
-    
-    with measure_duration(f'plotting track {track.index}'):
-        plt.plot(all_chunks)
+    plt.margins(0, 0)
+    color = random.choice(["r", "g", "b", "c", "m", "y"])
 
-    with measure_duration(f'saving figure {track.index}'):
-        img_buffer = io.BytesIO()
-        plt.savefig(img_buffer, format='png', dpi=100, bbox_inches='tight', pad_inches=0)
+    plt.plot(all_chunks, color=color, marker="", linewidth=0.2)
+
+    img_buffer = io.BytesIO()
+    plt.savefig(img_buffer, format="png", dpi=100, bbox_inches="tight", pad_inches=0)
 
     img_buffer.seek(0)
     return StreamingResponse(img_buffer, media_type="image/png")
