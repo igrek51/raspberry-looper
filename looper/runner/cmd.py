12e4690da906d42489acb005f4bc3e96999377e3
igrek51 <igrek51.dev@gmail.com>
2022-05-12 18:11:41 +0200

    Terminate child processes on exit

--- looper/runner/cmd.py
+++ looper/runner/cmd.py
@@ -1,12 +1,12 @@
 import io
-import os
+import signal
 import sys
 import select
 import subprocess
 from typing import Callable
 import threading
 import time
-import shlex
+import psutil
 
 from nuclear.shell import CommandError
 from nuclear.sublog import log
@@ -23,6 +23,7 @@ class BackgroundCommand:
         """Run system shell command in background."""
         self._stop: bool = False
         self._captured_stream = io.StringIO()
+        self._debug = debug
 
         def monitor_output(stream: BackgroundCommand):
             stdout_iter = iter(stream._process.stdout.readline, b'')
@@ -65,19 +66,29 @@ class BackgroundCommand:
 
         if debug:
             log.debug(f'Command: {cmd}')
-        self._process = subprocess.Popen(shlex.split(cmd), 
-            stdout=subprocess.PIPE, 
-            stderr=subprocess.STDOUT, 
-            shell=False,
-            preexec_fn=os.setsid,
+        self._process = subprocess.Popen(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            shell=True,
         )
 
         self._monitor_thread.start()
 
     def terminate(self):
         self._stop = True
+
+        parent = psutil.Process(self._process.pid)
+        children = parent.children(recursive=False)
+        for child in children:
+            if self._debug:
+                log.debug(f'terminating child process', pid=child.pid)
+            child.send_signal(signal.SIGTERM)
+
         self._process.terminate()
         self._process.poll()  # wait for subprocess
+        if self._debug:
+            log.debug(f'subprocess terminated', pid=self._process.pid)
         self._monitor_thread.join()  # wait for thread is finished
 
     def wait(self):
